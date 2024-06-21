d00fec11f49b5e639873dec553ee79a889109747
jxxvxs <jarvis.5211011032@student.uty.ac.id>
2024-06-21 19:15:49 +0700

    fix: resolve the previous error, by adding ` export const runtime = "edge";`

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,6 +1,8 @@
 import AddContentMessages from '@/components/form/add-content'
 import TitleBanner from '@/components/layout/banner-add';
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto p-10 flex flex-col justify-center">
